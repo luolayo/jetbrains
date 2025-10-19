6eab5048c357c56a55edcf9c62082c9fd9562234
luola <me@luola.me>
2025-10-19 16:41:02 +0800

    1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,21 @@
 <script lang="ts" setup>
 import {useDrvice} from "./store/useDrvice";
 import {Actions, Clean} from "../wailsjs/go/main/App";
+import { ElMessage } from 'element-plus'
 
 const drviceStore = useDrvice();
 drviceStore.getDrviceInfo()
 
 const actions = async () => {
  const data = await Actions();
+ 
   console.log(data);
 }
 
 const clean = async () => {
  const data = await Clean();
   console.log(data);
+  ElMessage(data)
 }
 </script>
 
